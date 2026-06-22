f3b8e59f0a0c4235eac0cc0065aa8a4102a5552a
Adair Kevin <101688536+AdairKevin@users.noreply.github.com>
2026-06-22 13:01:43 -0600

    Final

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const DATE_TARGET = new Date("12/20/2025 04:00 PM");
+  const DATE_TARGET = new Date("08/12/2026 04:30 PM");
 
   const SPAN_DAYS = document.querySelector("span#days");
   const SPAN_HOURS = document.querySelector("span#hours");
@@ -16,13 +16,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const DURATION = DATE_TARGET - NOW;
     const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
     const REMAINING_HOURS = Math.floor(
-      (DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR
+      (DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR,
     );
     const REMAINING_MINUTES = Math.floor(
-      (DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE
+      (DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE,
     );
     const REMAINING_SECONDS = Math.floor(
-      (DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND
+      (DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND,
     );
 
     SPAN_DAYS.textContent = REMAINING_DAYS;
